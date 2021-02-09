61d3cf26cb0af89ac233b0ea5c3fdbb3b29a9908
Sasha Ostrovnoy <Zayacz.sit@gmail.com>
2021-02-09 09:54:04 +0300

    Шарага КСт

---
+++ Arenda/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arenda
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArendaEntities DB = new ArendaEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void update()
+        {
+
+        }
+
+        private void btlog_Click(object sender, RoutedEventArgs e)
+        {
+            Agent agent = DB.Agent.Where(x => x.Login == TBlog.Text && x.Password == PBpass.Password).SingleOrDefault();
+            if (agent == null)
+            {
+                MessageBox.Show("Такого Пользователя нет");
+            }
+            else if (agent.Post == "Старший агент")
+            {
+                Windows.Object contract = new Windows.Object();
+                contract.Show();
+                this.Close();
+            }
+
+
+    }
+    }
+}
